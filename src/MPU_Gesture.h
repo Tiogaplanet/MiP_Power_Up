d3d3e608eff3bbc4968bf3bb0df25a3e70a62b42
Tiogaplanet <samuel.trassare@gmail.com>
2026-09-01 10:03:51 -0400

    typo

--- src/MPU_Gesture.h
+++ src/MPU_Gesture.h
@@ -29,7 +29,7 @@ class MiP;
 enum MiPGestureMode : uint8_t {
   MIP_GESTURE_RADAR_DISABLED = 0x00,  ///< Both gesture detection and radar
                                       ///< tracking modes are disabled.
-  MIP_GESTURE = 0x02                  ///< Gesture detection mode is active.
+  MIP_GESTURE = 0x02,                  ///< Gesture detection mode is active.
   MIP_RADAR = 0x04                  ///< Sensor is in Radar mode.
 };
 
